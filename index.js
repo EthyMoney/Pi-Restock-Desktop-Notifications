c0615985a63c014c4a4e4534bdd4592b18dae611
Logan <ultimate360solutions@gmail.com>
2023-07-06 15:07:05 -0500

    improved multi-alert handling and added links

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ const cron = require('node-cron');
 const path = require('path');
 
 let parser = new Parser();
-let latestTitle;
+let latestItems = [];
 
-const notify = (title, content) => {
+const notify = (title, content, link) => {
   notifier.notify({
     title: title,
     message: content,
+    open: link,
     sound: false,
     wait: true,
-    appID: 'Pi Stock Notifier', // the tiny subtitle on the notifications (snoretoast puts it there by default, this overrides it)
+    appID: 'Pi Stock Notifier',
     icon: path.join(__dirname, 'assets/raspberrry_pi_logo.png'),
   });
 };
@@ -21,21 +22,27 @@ notify('Pi Stock Notifier', 'Pi Stock Notifier is running, will notify you when 
 
 async function checkFeed(initialCheck = false) {
   let feed;
-  try { // catch any http errors with the request
+  try {
     feed = await parser.parseURL('https://rpilocator.com/feed/');
   } catch (e) {
     console.log('Error fetching feed, will try again on next interval');
     return;
   }
 
-  if (latestTitle !== feed.items[0].title) {
-    latestTitle = feed.items[0].title;
-    if (latestTitle.includes('Stock Alert (US)') && !initialCheck) {
-      const trimmedTitle = latestTitle.substring(17);
-      notify('Raspberry Pi\'s IN STOCK!', trimmedTitle);
+  // Get new items since last check
+  let newItems = feed.items.filter(item => !latestItems.find(i => i.title === item.title));
+
+  // Update the latestItems array
+  latestItems = feed.items.map(item => ({ title: item.title, link: item.link }));
+
+  // Iterate over new items and notify if they match the condition
+  newItems.forEach(item => {
+    if (item.title.includes('Stock Alert (US)') && !initialCheck) {
+      const trimmedTitle = item.title.substring(17);
+      notify('Raspberry Pi\'s IN STOCK!', trimmedTitle, item.link);
       console.log('Raspberry Pi\'s IN STOCK! Check your notifications!')
     }
-  }
+  });
 };
 
 // Run the task every minute
